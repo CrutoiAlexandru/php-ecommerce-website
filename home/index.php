e952774347bb384d2df7932f40b5fe019e0b76af
Alexandru-Vitali.Crutoi.76010082 <Alexandru-Vitali.Crutoi@microfocus.com>
2022-11-28 01:57:07 +0200

    Update styling

--- home/index.php
+++ home/index.php
@@ -6,11 +6,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="/global/style.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href='https://css.gg/shopping-cart.css' rel='stylesheet'>
 </head>
 
 <body>
-    Welcome home
+    <div>
+        <h2>
+            <a href="/home">
+                <span>
+                    Pitshop
+                </span>
+            </a>
+        </h2>
+        <div>
+            <h2 style="margin-right: 2vw;">About us</h2>
+            <i class="gg-shopping-cart"></i>
+            <h2>Profile</h2>
+        </div>
+    </div>
+
+    <div style="width: 100vw; display:flex; justify-content: center;">
+        <div class="line"></div>
+    </div>
+
+    <div class="grid">
+        <h1>hello</h1>
+        <h1>hi</h1>
+        <h1>third</h1>
+    </div>
 </body>
 
 </html>
--- login/index.php
+++ login/index.php
@@ -6,21 +6,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log In</title>
+    <link rel="stylesheet" href="/global/style.css">
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <h1>Welcome</h1>
+    <div style="height: 11.5vh;"></div>
 
-    <form action="/src/auth.php" method="post">
-        <label for="username">Username</label>
-        <input type="text" name="username">
+    <div>
+        <form action="/src/auth.php" method="post">
+            <label for="username">Username</label>
+            <input type="text" name="username">
 
-        <label for="password">Password</label>
-        <input type="password" name="password">
+            <label for="password">Password</label>
+            <input type="password" name="password">
 
-        <button>Submit</button>
-    </form>
+            <button>Submit</button>
+        </form>
+
+        <h1><span>Pitshop</span><br>Your pitstop, for a quick shop</h1>
+    </div>
 
     <div style="height: 10px;"></div>
 
